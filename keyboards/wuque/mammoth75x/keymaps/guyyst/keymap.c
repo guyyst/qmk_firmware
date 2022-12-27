959d62769182d6d5f52131462a85e1ac64082368
Yannic <yannic.mie@gmail.com>
2022-12-27 05:05:50 +0100

    Add mammoth kay map

---
+++ keyboards/wuque/mammoth75x/keymaps/guyyst/keymap.c
@@ -0,0 +1,64 @@
+/* Copyright 2021 wuquestudio
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    [0] = LAYOUT_all(
+        KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_INS,           KC_VOLU, KC_MUTE,KC_VOLD,
+        KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_BSPC,
+        KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,               KC_DEL, KC_PGUP,
+        KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,                KC_DEL, KC_PGDN,
+        KC_LSFT, KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_RSFT,               KC_UP,
+        KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,           KC_SPC,           KC_SPC,  KC_MEDIA_PREV_TRACK,  KC_MEDIA_PLAY_PAUSE,  KC_MEDIA_NEXT_TRACK, KC_LEFT, KC_DOWN, KC_RGHT
+    ),
+    [1] = LAYOUT_all(
+        QK_BOOT,  KC_F13,  KC_F14,  KC_F15,  KC_F16,  KC_F17,  KC_F18,  KC_F19,  KC_F20,  KC_F21,  KC_F22,  KC_F23,  KC_F24, _______,          _______, _______, _______,
+        KC_NUM, KC_KP_1, KC_KP_2, KC_KP_3, KC_KP_4, KC_KP_5, KC_KP_6, KC_KP_7, KC_KP_8, KC_KP_9, KC_KP_0, KC_KP_MINUS, KC_KP_PLUS, _______, _______,
+        _______, _______, _______, RALT(KC_5), _______, _______, _______, RALT(KC_Y), _______, RALT(KC_P), _______, _______, _______, _______,               _______, _______,
+        _______, RALT(KC_Q), RALT(KC_S), _______, _______, _______, _______, _______, _______, _______, _______, _______,          _______,               _______, _______,
+        _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,               _______,
+        _______, _______, _______,          _______,          _______,          _______,          _______, _______, _______,               _______, _______, _______
+    )
+};
+
+bool caps_used_as_fn = false;
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+
+    if (record->event.pressed) {
+        caps_used_as_fn = true;
+    }
+
+    switch (keycode) {
+       case KC_CAPS:
+            if (record->event.pressed) {
+                layer_on(1);
+                caps_used_as_fn = false;
+            } else {
+                layer_off(1);
+                if (!caps_used_as_fn) {
+                    register_code(KC_CAPS);
+                    unregister_code(KC_CAPS);
+                }
+            }
+            return false;
+        default:
+            break;
+    }
+
+    return true;
+}
